bfd1bac77241218d45547d8a2b420fb24e29f4a3
Blazing Ice <bvvr2007@gmail.com>
2026-07-03 12:26:24 +0530

    used functions

--- PingPong/main.cpp
+++ PingPong/main.cpp
@@ -1,7 +1,18 @@
 #include <SFML/Graphics.hpp>
 
+void paddleControl(sf::RectangleShape& rightPaddle, sf::RectangleShape& leftPaddle, float paddleSpeed, float dt, int HEIGHT, float paddleHeight);
+void ballMovement(sf::CircleShape& ball, float ballSpeedX, float& ballSpeedY, float dt, float HEIGHT, float ballDiameter);
+void paddleCollision(sf::RectangleShape& leftPaddle, sf::RectangleShape& rightPaddle, sf::CircleShape& ball, float& ballSpeedX);
+void playerMiss(sf::CircleShape& ball, int& leftPlayerScore, int& rightPlayerScore, float ballRadius, float ballDiameter, int WIDTH, int HEIGHT, float& ballSpeedY);
+void updateScoreText(sf::Text& leftPlayerScoreText, sf::Text& rightPlayerScoreText, int leftPlayerScore, int rightPlayerScore);
+void draw(sf::RenderWindow& window, sf::RectangleShape& leftPaddle, sf::RectangleShape& rightPaddle, sf::CircleShape& ball, sf::Text& leftPlayerScoreText, sf::Text& rightPlayerScoreText);
+
+
+
 int main()
 {
+	const int WIDTH = 1280, HEIGHT = 720;
+
 	sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "Ping-Pong Game");
 	sf::Clock clock;
 	sf::Font font("LowresPixel-Regular.otf");
@@ -18,12 +29,10 @@ int main()
 	rightPlayerScoreText.setPosition({ 730.f, 20.f });
 	rightPlayerScoreText.setFillColor(sf::Color::White);
 
-	const int WIDTH = 1280, HEIGHT = 720;
-
 	//--------------------------------------------------------------------------------------------------
 	//PLAYER SCORES
 	//--------------------------------------------------------------------------------------------------
-	int leftPlayerScore = 0, rightPlayerScore = 0;
+	int leftPlayerScore = 100000, rightPlayerScore = 0;
 
 	//--------------------------------------------------------------------------------------------------
 	//PADDLE SETTINGS
@@ -58,81 +67,113 @@ int main()
 		}
 
 		float dt = clock.restart().asSeconds();
-		float rightPaddleY = rightPaddle.getPosition().y;
-		float leftPaddleY = leftPaddle.getPosition().y;
-		window.clear(sf::Color::Black);
 
+		paddleControl(rightPaddle, leftPaddle, paddleSpeed, dt, HEIGHT, paddleHeight);
+		
+		ballMovement(ball, ballSpeedX, ballSpeedY, dt, HEIGHT, ballDiameter);
+		
+		paddleCollision(leftPaddle, rightPaddle, ball, ballSpeedX);
+		
+		playerMiss(ball, leftPlayerScore, rightPlayerScore, ballRadius, ballDiameter, WIDTH, HEIGHT, ballSpeedY);
 
-		//--------------------------------------------------------------------------------------------------
-		//PADDLE CONTROL
-		//--------------------------------------------------------------------------------------------------
-		//RIGHT PADDLE CONTROL
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) && rightPaddleY > 0) {
-			rightPaddle.move({ 0, -paddleSpeed * dt });
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) && rightPaddleY < HEIGHT - paddleHeight) {
-			rightPaddle.move({ 0, paddleSpeed * dt });
-		}
-		// LEFT PADDLE CONTROL
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) && leftPaddleY > 0) {
-			leftPaddle.move({ 0, -paddleSpeed * dt });
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S) && leftPaddleY < HEIGHT - paddleHeight) {
-			leftPaddle.move({ 0, paddleSpeed * dt });
-		}
+		updateScoreText(leftPlayerScoreText, rightPlayerScoreText, leftPlayerScore, rightPlayerScore);
 
-		//--------------------------------------------------------------------------------------------------
-		//BALL MOVEMENT
-		//--------------------------------------------------------------------------------------------------
-		ball.move({ ballSpeedX * dt, ballSpeedY * dt });
-		if (ball.getPosition().y <= 0 || ball.getPosition().y >= HEIGHT - ballDiameter) {
-			ballSpeedY = -ballSpeedY;
-		}
+		draw(window, leftPaddle, rightPaddle, ball, leftPlayerScoreText, rightPlayerScoreText);
 
-		//--------------------------------------------------------------------------------------------------
-		//CHECK BALL AND PADDLE COLLISION OR IF BALL MISSES ANY ONE PADDLE
-		//--------------------------------------------------------------------------------------------------
-		//PADDLE COLLISION
-		if (ballSpeedX < 0 && leftPaddle.getGlobalBounds().findIntersection(ball.getGlobalBounds()))
-		{
-			ballSpeedX = -ballSpeedX;
-		}
-		if (ballSpeedX > 0 && rightPaddle.getGlobalBounds().findIntersection(ball.getGlobalBounds()))
-		{
-			ballSpeedX = -ballSpeedX;
-		}
-
-		//IF BALL MISSES A PADDLE
-		if (ball.getPosition().x < 0 - ballDiameter)
-		{
-			rightPlayerScore++;
-			ball.setPosition({ WIDTH / 2.f - ballRadius, HEIGHT / 2.f - ballRadius });
-			ballSpeedY = 200.f;
-
-		}
-		if (ball.getPosition().x > WIDTH)
-		{
-			leftPlayerScore++;
-			ball.setPosition({ WIDTH / 2.f - ballRadius, HEIGHT / 2.f - ballRadius });
-			ballSpeedY = 200.f;
-		}
-
-		//--------------------------------------------------------------------------------------------------
-		//UPDATE PLAYER SCORES TEXT
-		//--------------------------------------------------------------------------------------------------
-		leftPlayerScoreText.setString(std::to_string(leftPlayerScore));
-		rightPlayerScoreText.setString(std::to_string(rightPlayerScore));
-
-		//--------------------------------------------------------------------------------------------------
-		// DRAWING
-		//--------------------------------------------------------------------------------------------------
-		window.draw(leftPaddle);
-		window.draw(rightPaddle);
-		window.draw(ball);
-		window.draw(leftPlayerScoreText);
-		window.draw(rightPlayerScoreText);
-
-		window.display();
 	}
 	return 0;
+}
+
+//--------------------------------------------------------------------------------------------------
+//PADDLE CONTROL
+//--------------------------------------------------------------------------------------------------
+void paddleControl(sf::RectangleShape& rightPaddle, sf::RectangleShape& leftPaddle, float paddleSpeed,float dt, int HEIGHT, float paddleHeight)
+{
+	float rightPaddleY = rightPaddle.getPosition().y;
+	float leftPaddleY = leftPaddle.getPosition().y;
+	//RIGHT PADDLE CONTROL
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) && rightPaddleY > 0) {
+		rightPaddle.move({ 0, -paddleSpeed * dt });
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) && rightPaddleY < HEIGHT - paddleHeight) {
+		rightPaddle.move({ 0, paddleSpeed * dt });
+	}
+	// LEFT PADDLE CONTROL
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) && leftPaddleY > 0) {
+		leftPaddle.move({ 0, -paddleSpeed * dt });
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S) && leftPaddleY < HEIGHT - paddleHeight) {
+		leftPaddle.move({ 0, paddleSpeed * dt });
+	}
+}
+
+//--------------------------------------------------------------------------------------------------
+//BALL MOVEMENT
+//--------------------------------------------------------------------------------------------------
+void ballMovement(sf::CircleShape& ball, float ballSpeedX, float& ballSpeedY, float dt, float HEIGHT, float ballDiameter)
+{
+	ball.move({ ballSpeedX * dt, ballSpeedY * dt });
+	if (ball.getPosition().y <= 0 || ball.getPosition().y >= HEIGHT - ballDiameter) {
+		ballSpeedY = -ballSpeedY;
+	}
+}
+
+//--------------------------------------------------------------------------------------------------
+//CHECK BALL - PADDLE COLLISION
+//--------------------------------------------------------------------------------------------------
+void paddleCollision(sf::RectangleShape& leftPaddle, sf::RectangleShape& rightPaddle, sf::CircleShape& ball, float& ballSpeedX)
+{
+	if (ballSpeedX < 0 && leftPaddle.getGlobalBounds().findIntersection(ball.getGlobalBounds()))
+	{
+		ballSpeedX = -ballSpeedX;
+	}
+	if (ballSpeedX > 0 && rightPaddle.getGlobalBounds().findIntersection(ball.getGlobalBounds()))
+	{
+		ballSpeedX = -ballSpeedX;
+	}
+}
+
+//--------------------------------------------------------------------------------------------------
+//IF PLAYER MISSES TO HIT THE BALL, UPDATE SCORE AND RESET BALL POSITION
+//--------------------------------------------------------------------------------------------------
+void playerMiss(sf::CircleShape& ball, int& leftPlayerScore, int& rightPlayerScore,float ballRadius, float ballDiameter, int WIDTH, int HEIGHT, float& ballSpeedY)
+{
+	if (ball.getPosition().x < 0 - ballDiameter)
+	{
+		rightPlayerScore++;
+		ball.setPosition({ WIDTH / 2.f - ballRadius, HEIGHT / 2.f - ballRadius });
+		ballSpeedY = 200.f;
+
+	}
+	if (ball.getPosition().x > WIDTH)
+	{
+		leftPlayerScore++;
+		ball.setPosition({ WIDTH / 2.f - ballRadius, HEIGHT / 2.f - ballRadius });
+		ballSpeedY = 200.f;
+	}
+}
+
+//--------------------------------------------------------------------------------------------------
+//UPDATE PLAYER SCORES TEXT
+//--------------------------------------------------------------------------------------------------
+void updateScoreText(sf::Text& leftPlayerScoreText, sf::Text& rightPlayerScoreText, int leftPlayerScore, int rightPlayerScore)
+{
+	leftPlayerScoreText.setString(std::to_string(leftPlayerScore));
+	rightPlayerScoreText.setString(std::to_string(rightPlayerScore));
+}
+
+//--------------------------------------------------------------------------------------------------
+// DRAW
+//--------------------------------------------------------------------------------------------------
+void draw(sf::RenderWindow& window, sf::RectangleShape& leftPaddle, sf::RectangleShape& rightPaddle, sf::CircleShape& ball, sf::Text& leftPlayerScoreText, sf::Text& rightPlayerScoreText)
+{
+	window.clear(sf::Color::Black);
+
+	window.draw(leftPaddle);
+	window.draw(rightPaddle);
+	window.draw(ball);
+	window.draw(leftPlayerScoreText);
+	window.draw(rightPlayerScoreText);
+
+	window.display();
 }
